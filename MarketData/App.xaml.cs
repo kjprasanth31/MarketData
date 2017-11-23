f2aa15a84571503f21553d3c4d712fd8b2027bd3
Prasanth Kuruvilla John <kjprasanth31@gmail.com>
2017-11-23 19:13:49 +0000

    first commit

---
+++ MarketData/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using Unity;
+namespace MarketData
+{
+    /// <summary>
+    /// Interaction logic nfor App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            Initialise();
+        }
+
+        void Initialise()
+        {
+            var container = new UnityContainer();
+        }
+    }
+}
