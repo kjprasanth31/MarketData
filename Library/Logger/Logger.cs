825e5ef13af6bcc746adb50ef93a3ab431f0f0cb
Prasanth Kuruvilla John <kjprasanth31@gmail.com>
2017-11-25 13:09:41 +0000

    logger

---
+++ Library/Logger/Logger.cs
@@ -0,0 +1,36 @@
+﻿using NLog;
+
+namespace Library
+{
+    public class Logger : ILogger
+    {
+        private readonly NLog.Logger _logger;
+
+        public Logger()
+        {
+            _logger = LogManager.GetLogger("Generic");
+        }
+
+        public void LogError(string error)
+        {
+            _logger.Error(error);
+        }
+
+        public void LogInfo(string info)
+        {
+            _logger.Info(info);
+        }
+
+        public void LogWarning(string warning)
+        {
+            _logger.Warn(warning);
+        }
+    }
+
+    public interface ILogger
+    {
+        void LogInfo(string info);
+        void LogWarning(string warning);
+        void LogError(string error);
+    }
+}
