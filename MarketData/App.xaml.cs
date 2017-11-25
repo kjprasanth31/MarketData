792408af55923350e35a90cf3a35928ca2451b6a
Prasanth Kuruvilla John <kjprasanth31@gmail.com>
2017-11-25 13:04:50 +0000

    add window service

--- MarketData/App.xaml.cs
+++ MarketData/App.xaml.cs
@@ -18,21 +18,6 @@ namespace MarketData
         {
             var container = new UnityContainer();
             container.RegisterInstance<ILogger>(new Logger());
-            container.RegisterInstance<ISomeClass>(new SomeClass(container.Resolve<ILogger>()));
-            var k = container.Resolve<ISomeClass>();
         }
-    }
-
-    public class SomeClass : ISomeClass
-    {
-        public SomeClass(ILogger logger)
-        {
-            logger.LogInfo("Some Info");
-            logger.LogWarning("Some Warning");
-        }
-    }
-
-    public interface ISomeClass
-    {
     }
 }
--- MarketData/MainWindow.xaml.cs
+++ MarketData/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using Prism.Commands;
+using System.Windows;
+using WindowHelper;
 
 namespace MarketData
 {
@@ -10,6 +12,34 @@ namespace MarketData
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = new MainWindowController().ViewModel;
+        }
+    }
+
+    public class MainWindowViewModel
+    {
+        public DelegateCommand<string> NewWindowCommand { get; set; }
+        public MainWindowViewModel()
+        {
+        }
+    }
+
+    public class MainWindowController
+    {
+        public MainWindowViewModel ViewModel;
+        private readonly WindowBuilderService _windowBuilderService;
+
+        public MainWindowController()
+        {
+            ViewModel = new MainWindowViewModel { NewWindowCommand = new DelegateCommand<string>(title => CreateNewWindow(title), _ => true) };
+            _windowBuilderService = new WindowBuilderService();
+        }
+
+        private void CreateNewWindow(string title)
+        {
+            _windowBuilderService
+                .Title(title)
+                .Show();
         }
     }
 }
