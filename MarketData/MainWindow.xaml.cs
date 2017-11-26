44c16773dac533e9834cd7836f0c7ed1861d5dc3
Prasanth Kuruvilla John <kjprasanth31@gmail.com>
2017-11-26 20:46:27 +0000

    SHELL

--- MarketData/MainWindow.xaml.cs
+++ MarketData/MainWindow.xaml.cs
@@ -1,6 +1,4 @@
-﻿using Prism.Commands;
-using System.Windows;
-using WindowHelper;
+﻿using System.Windows;
 
 namespace MarketData
 {
@@ -12,34 +10,6 @@ namespace MarketData
         public MainWindow()
         {
             InitializeComponent();
-            this.DataContext = new MainWindowController().ViewModel;
-        }
-    }
-
-    public class MainWindowViewModel
-    {
-        public DelegateCommand<string> NewWindowCommand { get; set; }
-        public MainWindowViewModel()
-        {
-        }
-    }
-
-    public class MainWindowController
-    {
-        public MainWindowViewModel ViewModel;
-        private readonly WindowBuilderService _windowBuilderService;
-
-        public MainWindowController()
-        {
-            ViewModel = new MainWindowViewModel { NewWindowCommand = new DelegateCommand<string>(title => CreateNewWindow(title), _ => true) };
-            _windowBuilderService = new WindowBuilderService();
-        }
-
-        private void CreateNewWindow(string title)
-        {
-            _windowBuilderService
-                .Title(title)
-                .Show();
         }
     }
 }
